ba1ba61f57c1f4c1af304d63da9da60cff61ae84
Sharma <BAM20294@superioruniform.com>
2021-03-05 11:30:47 +0530

    s3 initial commit added

--- main.tf
+++ main.tf
@@ -15,3 +15,8 @@ module "security-group" {
   vpc_id = ""
   name = ""
 }
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "1.20.0"
+  # insert the 5 required variables here
+}
